6eedcecb5c668c6aa4611de929dab509776af6f3
Andria <andria.inas@gmail.com>
2024-03-28 17:09:39 +0400

    first commit

---
+++ PokeDexplorer/PokeDexplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexplorerApp.swift
+//  PokeDexplorer
+//
+//  Created by Andria Inasaridze on 28.03.24.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
